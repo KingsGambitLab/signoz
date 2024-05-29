0b30dee08371fd61362d9a784bd46d30a46a7ed1
ngudbhav <ngudbhav05@hotmail.com>
2024-05-29 13:28:55 +0530

    fix: Migrate from EBS to EFS

--- .platform/hooks/prebuild/01_mount_volumes.sh
+++ .platform/hooks/prebuild/01_mount_volumes.sh
@@ -11,12 +11,11 @@ CLICKHOUSE_VOLUME_ID=$(/opt/elasticbeanstalk/bin/get-config environment -k CLICK
 QUERY_VOLUME_ID=$(/opt/elasticbeanstalk/bin/get-config environment -k QUERY_SERVICE_VOLUME)
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 attach-volume --region ap-south-1 --volume-id $CLICKHOUSE_VOLUME_ID --instance-id $INSTANCE_ID --device /dev/sdh
 aws ec2 attach-volume --region ap-south-1 --volume-id $QUERY_VOLUME_ID --instance-id $INSTANCE_ID --device /dev/sdi
 echo "EBS Volums attached. Waiting for 10 seconds before mounting."
 sleep 10
 mkdir -p $CLICKHOUSE_DATA_PATH
-mount /dev/sdh $CLICKHOUSE_DATA_PATH
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $CLICKHOUSE_VOLUME_ID.efs.ap-south-1.amazonaws.com:/ $CLICKHOUSE_DATA_PATH
 mkdir -p $QUERY_DATA_PATH
 mount /dev/sdi $QUERY_DATA_PATH
 echo "EBS Volume mounted"
