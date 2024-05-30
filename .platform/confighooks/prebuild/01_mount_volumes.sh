ef2bde54943f0eaddab1da2ccef6f7f1f5c2ebf0
Udbhav Gambhir <udbhav@scaler.com>
2024-05-30 04:26:10 -0700

    feat: Code changes for EB deployment (#1)
    * feat: Code changes for EB deployment
    
    * fix: add execute permissions
    
    * fix: missed volume creation :p
    
    * fix: sync hooks with confighooks
    
    * fix: values
    
    * bump version
    
    * fix: Remove sample application
    
    * fix: Migrate from EBS to EFS
    
    * set ENV variable
    
    * nit
    
    * use efs
    
    * nit
    
    * revert efs and increase max query size
    
    * nit

---
+++ .platform/confighooks/prebuild/01_mount_volumes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Mount the EBS Volume from ENV variable
+echo "Creating docker volumes"
+docker volume create --name=signoz-clickhouse
+docker volume create --name=signoz-query-service
+CLICKHOUSE_DATA_PATH=/var/lib/docker/volumes/signoz-clickhouse
+QUERY_DATA_PATH=/var/lib/docker/volumes/signoz-query-service
+echo "Mounting EBS Volumes"
+CLICKHOUSE_VOLUME_ID=$(/opt/elasticbeanstalk/bin/get-config environment -k CLICKHOUSE_VOLUME)
+QUERY_VOLUME_ID=$(/opt/elasticbeanstalk/bin/get-config environment -k QUERY_SERVICE_VOLUME)
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+aws ec2 attach-volume --region ap-south-1 --volume-id $CLICKHOUSE_VOLUME_ID --instance-id $INSTANCE_ID --device /dev/sdh
+aws ec2 attach-volume --region ap-south-1 --volume-id $QUERY_VOLUME_ID --instance-id $INSTANCE_ID --device /dev/sdi
+echo "EBS Volums attached. Waiting for 10 seconds before mounting."
+sleep 10
+mkdir -p $CLICKHOUSE_DATA_PATH
+mount /dev/sdh $CLICKHOUSE_DATA_PATH
+mkdir -p $QUERY_DATA_PATH
+mount /dev/sdi $QUERY_DATA_PATH
+echo "EBS Volume mounted"
+if find $CLICKHOUSE_DATA_PATH -maxdepth 0 -empty | read v; then mkfs -t ext4 /dev/sdh; fi
+if find $QUERY_DATA_PATH -maxdepth 0 -empty | read v; then mkfs -t ext4 /dev/sdi; fi
+echo "EBS Volume formatted"
